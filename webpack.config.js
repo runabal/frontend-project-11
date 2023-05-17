c2305cb86e54da0c41ff3efe229c6dba733689ef
runabal <nvbalandina@mail.ru>
2023-05-17 22:40:24 +0300

    step1

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-//const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
 
 const isProduction = process.env.NODE_ENV == "production";
 
