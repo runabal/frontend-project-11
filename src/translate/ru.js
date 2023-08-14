2dc679b9abb595b129e502960095567b9425e99e
runabal <nvbalandina@mail.ru>
2023-08-14 14:13:11 +0300

    refactoring

--- src/translate/ru.js
+++ src/translate/ru.js
@@ -1,16 +1,16 @@
 export default {
   translation: {
     errors: {
-      urlError: 'Ссылка должна быть валидным URL',
-      alreadyExist: 'RSS уже существует',
-      rssError: 'Ресурс не содержит валидный RSS',
-      networkError: 'Ошибка сети, попробуйте позже',
-      somethingWrong: 'Что-то пошло не так',
+      urlError: "Ссылка должна быть валидным URL",
+      alreadyExist: "RSS уже существует",
+      rssError: "Ресурс не содержит валидный RSS",
+      networkError: "Ошибка сети, попробуйте позже",
+      somethingWrong: "Что-то пошло не так",
     },
-    loading: 'Идет загрузка',
-    success: 'RSS успешно загружен',
-    posts: 'Посты',
-    feeds: 'Фиды',
-    inspect: 'Просмотр',
+    loading: "Идет загрузка",
+    success: "RSS успешно загружен",
+    posts: "Посты",
+    feeds: "Фиды",
+    inspect: "Просмотр",
   },
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,61 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const {fileUrlToPath} = require('url');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-
-const filename = __filename;
-const dirname = path.dirname(filename);
-const isProduction = process.env.NODE_ENV == 'production';
+const isProduction = process.env.NODE_ENV === "production";
 
 const config = {
-
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, "dist"),
   },
   devServer: {
     open: true,
-    host: 'localhost',
+    host: "localhost",
   },
   plugins: [
     new HtmlWebpackPlugin({
-    template: 'index.html',
-  }),
-   new MiniCssExtractPlugin(),
-],
+      template: "index.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env'],
+            presets: ["@babel/preset-env"],
           },
         },
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'style-loader', 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, "style-loader", "css-loader"],
       },
       {
         test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
       {
         test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: 'url-loader?limit=1000',
+        use: "url-loader?limit=1000",
       },
       {
         test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
-        use: 'file-loader',
+        use: "file-loader",
       },
     ],
   },
 };
 
-
-
-module.exports =() => {
+module.exports = () => {
   if (isProduction) {
-    config.mode = 'production';
-   } else {
-    config.mode = 'development';
+    config.mode = "production";
+  } else {
+    config.mode = "development";
   }
-   return config;
+  return config;
 };
